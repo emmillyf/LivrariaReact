8b5b323f0a0adbf31185a6b603c7e85f97ec6c51
Emmilly <mimig2250@gmail.com>
2024-06-10 13:43:20 -0300

    fix: corrigindo algumas palavras no code.

--- projetoFinal/src/pages/Ranking/index.jsx
+++ projetoFinal/src/pages/Ranking/index.jsx
@@ -5,10 +5,11 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 
-function rank() {  
+function Rank() {  
   const { id } = useParams();
   let navigate = useNavigate();
   const [avaliacoes, setAvaliacoes] = useState([]);
+  const { reset } = useForm();
 
   useEffect(() => {
     axios
@@ -23,7 +24,7 @@ function rank() {
     .get(`http://localhost:8080/publicacoes`)
     .then((response) => {
       const classificacoes = response.data.sort((a, b) => b.nota - a.nota);
-      setReviews(classificacoes);
+      setAvaliacoes(classificacoes);
     })
     .catch(() => console.log("Erro ao obter as avaliações!"));
   }, [id]);
@@ -34,16 +35,16 @@ function rank() {
         <div className="rank_card">
           <h1>Ranking de Livros</h1>
           <ul>
-          {avaliacoes.map(avaliador => (
-            <li key={avaliador.id}>
-              <h2>{avaliador.titulo}</h2>
-              <p>Nota:{avaliador.nota}</p>
-            </li>
-          ))}
-        </ul>
+              {avaliacoes.map(avaliador => (
+               <li key={avaliador.id}>
+                <h2>{avaliador.titulo}</h2>
+                <p>Nota:{avaliador.nota}</p>
+               </li>
+              ))}
+          </ul>
       </div>
     </div>
   );
 }
 
-export default rank;
+export default Rank;
